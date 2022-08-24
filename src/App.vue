2b3c3c594fbfea6075e2e262048f5aa6085fddab
Rafael Oviedo <oviedo.rp@gmail.com>
2022-08-23 22:13:17 -0300

    update(App): adding navbar and created hook

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavBar />
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
+import store from './store/index'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    NavBar
+  },
+  data() {
+    return {}
+  },
+  created() {
+    store.dispatch('getVideos')
+  },
 }
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+  /* -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
