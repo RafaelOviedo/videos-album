e6a41556ffafdac7c5848c73903727b2919cf17b
Rafael Oviedo <oviedo.rp@gmail.com>
2022-08-25 03:04:16 -0300

    update(Videos): changing laguage and adding custom pagination

--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -2,15 +2,28 @@
   <div>
     <div id="videosPage">
       <div id="formContainer">
-        <p id="inputLabel"><b>Añadir un nuevo video</b></p>
+        <p id="inputLabel"><b>Add new video</b></p>
         <form id="textInputContainer">
-          <input id="searchInput" type="text" v-model="inputText" placeholder="Añadir">
-          <button id="addButton" @click.prevent="addVideo"><b>Añadir</b></button>
+          <input id="searchInput" type="text" v-model="inputText" placeholder="Add">
+          <button id="addButton" @click.prevent="addVideo"><b>Add</b></button>
         </form>
       </div>
       
       <div id="videosContainer">
-          <VideoCard :item="item" v-for="(item, idx) in videos" :key="idx"/>
+          <VideoCard :item="item" v-for="(item) in videos.slice(numOfPage, numOfPage + 6)" :key="item.data.items[0].id"/>
+      </div>
+
+      <div id="prevNextCButtonsContainer">
+        <button 
+          class="leftAndRightButtons"
+          @click="previousPage" 
+          :disabled="numOfPage === 0"
+        >&#8592;</button>
+        <button
+          class="leftAndRightButtons"
+          @click="nextPage" 
+          :disabled="numOfPage === videos.length - 2"
+        >&#8594;</button>
       </div>
     </div>
   </div>
@@ -30,8 +43,15 @@ export default {
   data() {
     return {
       inputText: '',
-      mockData: [1, 2],
+      numOfPage: 0,
+      videosPerPage: 0,
     }
+  },
+  mounted() {
+    this.videosPerPage = this.videos.length - 2;
+  },
+  updated() {
+    this.videosPerPage = this.videos.length - 2;
   },
   watch: {
     videos() {
@@ -39,14 +59,23 @@ export default {
     }
   },
   methods: {
-      async addVideo() {
-        console.log("INPUT TEXT", this.inputText)
-            await axios.post('https://9f36sdoad4.execute-api.us-east-1.amazonaws.com/videos', { 
-                videoId: this.inputText
-            })
-            
-            this.inputText = ''
-        },
+    async addVideo() {
+        await axios.post('https://9f36sdoad4.execute-api.us-east-1.amazonaws.com/videos', { 
+            videoId: this.inputText
+        })
+        
+        this.inputText = ''
+    },
+    previousPage() {
+      if (this.numOfPage > 0) {
+          this.numOfPage = this.numOfPage - 2;
+      }
+    },
+    nextPage() {
+      if (this.numOfPage < this.videosPerPage) {
+          this.numOfPage = this.numOfPage + 2;
+      }
+    },
   },
   computed: {
     ...mapState(["videos"]),
@@ -58,13 +87,13 @@ export default {
 #videosPage {
   display: flex;
   flex-direction: column;
-  justify-content: space-evenly;
+  justify-content: center;
   align-items: center;
   position: absolute;
   top: 8vh;
   left: 0;
   width: 100vw;
-  height: 89vh;
+  height: 92vh;
   background: #f2f2f2;
 }
 #formContainer {
@@ -78,7 +107,7 @@ export default {
   justify-content: flex-start;
   align-items: center;
   width: 100%;
-  height: 100%;
+  height: 50%;
 }
 #inputLabel {
   font-size: 18px;
@@ -87,25 +116,41 @@ export default {
 }
 #searchInput {
   width: 70%;
-  height: 45%;
+  height: 90%;
   border: 1px solid #7B7B7B;
   border-radius: 5px 0 0 5px;
 }
 #addButton {
   width: 30%;
-  height: 51%;
+  height: 100%;
   color: #fff;
   background: #1469e6;
   border-radius: 0 5px 5px 0;
   border: none;
+  cursor: pointer;
 }
 
 #videosContainer {
   display: flex;
-  flex-direction: column;
+  flex-wrap: wrap;
+  flex-direction: row;
   justify-content: space-evenly;
   align-items: center;
-  width: 100vw;
-  height: 85%;
+  width: 50vw;
+  height: 70%;
+}
+
+#prevNextCButtonsContainer {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 15%;
+  height: 5%;
+}
+.leftAndRightButtons {
+  border: none;
+  background: transparent;
+  cursor: pointer;
+  font-size: 30px;
 }
 </style>
