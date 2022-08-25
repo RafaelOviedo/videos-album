563dcb8e7f2759ee0176e0ef8a481b3e4b466be6
Rafael Oviedo <oviedo.rp@gmail.com>
2022-08-24 22:35:12 -0300

    update(VideoCard): adding styles, close button and internal (VideoDetailModal) component

--- src/components/VideoCard.vue
+++ src/components/VideoCard.vue
@@ -1,19 +1,27 @@
 <template>
   <div id="videoCard">
-    <div id="imageContainer">
-      <img class="thumbImage" :src="item.data.items[0].snippet.thumbnails.high.url" alt="thumb">
+    <div id="imageContainer"> 
+      <img id="thumbImage" :src="item.data.items[0].snippet.thumbnails.high.url" alt="thumb" @click="openModal">
       <button id="deleteButton" @click="deleteVideo(item.data.items[0].id)">X</button>
       
-      <!-- <button id="deleteButton">X</button> -->
+      <transition name="fade">
+      <VideoDetailModal :item="item" v-if="isModalOpen"/>
+      </transition>
     </div>
   </div>
 </template>
 
 <script>
+import store from '../store/index'
 import axios from 'axios'
+import VideoDetailModal from './VideoDetailModal.vue'
+import { mapState } from 'vuex'
 
 export default {
   name: "VideoCard",
+  components: {
+    VideoDetailModal
+  },
   props: {
     item: Object,
   },
@@ -27,8 +35,14 @@ export default {
                 videoId: `https://www.youtube.com/watch?v=${elementId}`
             }
         })
-    }
+    },
+    openModal() {
+      store.commit('toggleModal')
+    },
   },
+  computed: {
+    ...mapState(['isModalOpen'])
+  }
 }
 </script>
 
@@ -37,27 +51,30 @@ export default {
   display: flex;
   justify-content: space-evenly;
   align-items: center;
-  width: 20%;
-  height: 25%;
-  /* background: #DFDFDF; */
+  width: 30%;
+  height: 30%;
 }
 
 #imageContainer {
   width: 80%;
   height: 100%;
   cursor: pointer;
-  /* border: 2px solid black; */
 }
-.thumbImage {
+#thumbImage {
   width: 100%;
   height: 100%;
-  border: 2px solid black;
 }
 #deleteButton {
     position: relative;
-    bottom: 100%;
+    bottom: 90%;
     left: 85%;
     cursor: pointer;
-    border: 1px solid black;
+    background: #000;
+    color: #fff;
+}
+
+#videoDetailLink {
+    width: 80px;
+    height: 80px;
 }
 </style>
