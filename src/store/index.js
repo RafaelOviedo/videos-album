aeef654e64720ccfcd8ec90c85a62072870fe407
Rafael Oviedo <oviedo.rp@gmail.com>
2022-08-25 02:20:25 -0300

    update(store): adding new states, actions and mutations

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,28 @@
 import { createStore } from "vuex";
 import axios from "axios";
+const apiKeyVar = process.env.VUE_APP_YOUTUBE_API_KEY3;
 
 const store = createStore({
     state: {
         videos: [],
+        videoId: '',
+        isReproducerOpen: false,
     },
 
     mutations: {
         setVideos(state, payload) {
             state.videos = payload;
         },
+        setVideoId(state, payload) {
+            state.videoId = payload;
+        },
+        toggleReproducer(state) {
+            state.isReproducerOpen = !state.isReproducerOpen;
+        },
     },
 
     actions: {
         async getVideos({ commit }) {
-            const apiKeyVar = process.env.VUE_APP_YOUTUBE_API_KEY3;
-
             let response = await axios.get('https://9f36sdoad4.execute-api.us-east-1.amazonaws.com/videos')
 
             let youtubeElements = []
@@ -24,8 +31,13 @@ const store = createStore({
                 let ids = response.data[i].videoId.slice(-11, response.data[i].videoId.length)
                 youtubeElements.push(await axios.get(`https://www.googleapis.com/youtube/v3/videos?id=${ids}&key=${apiKeyVar}&part=snippet`))
             }
-
                 commit('setVideos', youtubeElements)
+        },
+        toggleReproducer({ commit }) {
+            commit('toggleReproducer')
+        },
+        setVideoId({ commit }, payload) {
+            commit('setVideoId', payload)
         },
     },
 
