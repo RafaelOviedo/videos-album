fbe1939bf68fe1b89312a28e7a92adb2fd8a502d
Rafael Oviedo <oviedo.rp@gmail.com>
2022-08-31 22:46:18 -0300

    update(Videos component): adding logic to add correctly a youtube url

--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -9,9 +9,15 @@
         </form>
       </div>
       
+      <div id="spinner" v-if="isLoading" class="spinner"></div>
+
       <div id="videosContainer">
           <VideoCard :item="item" v-for="(item) in videos.slice(numOfPage, numOfPage + 6)" :key="item.data.items[0].id"/>
       </div>
+
+      <transition name="fade">
+      <Alert v-if="isErrorModalOpen" @closeErrorModal="closeErrorModal"/>
+      </transition>
 
       <div id="prevNextCButtonsContainer">
         <button 
@@ -32,26 +38,30 @@
 <script>
 import { mapState } from 'vuex';
 import VideoCard from '../components/VideoCard';
+import Alert from '../components/Alert';
 import store from '../store/index'
 import axios from 'axios'
 
 export default {
   name: "Videos",
   components: {
-    VideoCard
+    VideoCard,
+    Alert
   },
   data() {
     return {
       inputText: '',
+      isLoading: false,
       numOfPage: 0,
       videosPerPage: 0,
+      isErrorModalOpen: false,
     }
   },
   mounted() {
-    this.videosPerPage = this.videos.length - 2;
+    this.videosPerPage = this.videos.length - 6;
   },
   updated() {
-    this.videosPerPage = this.videos.length - 2;
+    this.videosPerPage = this.videos.length - 6;
   },
   watch: {
     videos() {
@@ -60,18 +70,36 @@ export default {
   },
   methods: {
     async addVideo() {
-        if(this.inputText.length < 43 || this.inputText.length > 43) {
-            alert(`The link format must be: https://www.youtube.com/watch?v= + videoId
+      let AWSendpoint = 'https://9f36sdoad4.execute-api.us-east-1.amazonaws.com/videos';
+      let baseUrl = 'https://www.youtube.com/watch?v=';
+      let baseUrl2 = 'https://youtu.be/';
+      let input = this.inputText;
 
-For example: https://www.youtube.com/watch?v=-_Yf3LhTQfw`)
+        if(!input.includes(baseUrl) || !input.includes(baseUrl2)) {
+            this.isErrorModalOpen = true,
 
             this.inputText = ''
         }
-        else {
-            await axios.post('https://9f36sdoad4.execute-api.us-east-1.amazonaws.com/videos', { 
-            videoId: this.inputText
-            })
-        
+
+        if(input.includes(baseUrl) && input.length >= 43) {
+            this.isErrorModalOpen = false
+            input = input.replace(input, input.slice(0, 43))
+
+            this.isLoading = true;
+            await axios.post(AWSendpoint, { videoId: input })
+            this.isLoading = false;
+            
+            this.inputText = ''
+        }
+
+        if(input.includes(baseUrl2) && input.length >= 28) {
+            this.isErrorModalOpen = false
+            input = input.replace(input, input.slice(0, 28))
+
+            this.isLoading = true;
+            await axios.post(AWSendpoint, { videoId: input })
+            this.isLoading = false;
+            
             this.inputText = ''
         }
     },
@@ -84,6 +112,9 @@ For example: https://www.youtube.com/watch?v=-_Yf3LhTQfw`)
       if (this.numOfPage < this.videosPerPage) {
           this.numOfPage = this.numOfPage + 2;
       }
+    },
+    closeErrorModal() {
+      this.isErrorModalOpen = false;
     },
   },
   computed: {
@@ -161,5 +192,9 @@ For example: https://www.youtube.com/watch?v=-_Yf3LhTQfw`)
   background: transparent;
   cursor: pointer;
   font-size: 30px;
+}
+#spinner {
+  top: 85%;
+  left: 90%;
 }
 </style>
